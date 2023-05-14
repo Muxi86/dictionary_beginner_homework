738fc3f5eacae85a86bc1a104a6fd53e27361943
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2023-05-14 19:44:08 +0500

    sav

--- sum_age_value.py
+++ sum_age_value.py
@@ -6,4 +6,15 @@ def sum_age_values(data:list) -> int:
     Returns:
         int: The sum of all age values in the dictionary
     """
-    return 
+    age = 0
+    for i in data:
+        age += i['age']
+    return age
+data = [{
+    'name': 'John',
+    'age': 27
+    },
+    {
+    'name': 'Mary',
+    'age': 42}]
+print(sum_age_values(data))
